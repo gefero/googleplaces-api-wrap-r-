870745083e25522e02c59f1fdfb21fd9904feea8
gfrosati <german.rosati@gmail.com>
2017-06-25 22:54:02 -0300

    Second commit

--- functions/google_places_api_wrapper.R
+++ functions/google_places_api_wrapper.R
@@ -19,7 +19,7 @@ library(NbClust)
 options(stringsAsFactors = F)
 
 # Radiuos centroid generation of ENAPROSS II
-setwd("/media/digitalhouse/Elements3/MTEySS/Local/grosati/Proyectos/Entorno ENAPROSS III/Cartografia")
+setwd("/media/digitalhouse/Elements3/MTEySS/Local/grosati/Proyectos/googleplaces-api-r/cartografia")
 dir<-dir(getwd(),".kml")
 #dir<-dir[-2]
 
@@ -29,12 +29,11 @@ radios = readOGR("Muestra conurbano final.kml", "Muestra conurbano final")
 #radios$gid<-radios$link_INDEC
 points <- gCentroid(radios, byid = TRUE)
 points_coords<-points@coords
+rm(radios,dir,points)
 
 library(RCurl)
 library(jsonlite)
 library(geosphere)
-
-
 
 ## Function formatting url for google places api
 # lat: latitude
@@ -50,31 +49,77 @@ format_url<-function(lat,long,radius,type,key){
 next_page<-function(json){
         if (!is.null(json$next_page_token)){
                 token<-json$next_page_token
-                url<-paste(url,"&pageToken",token,sep="")
-                return(url)
+                urll<-paste(urll,"&pageToken", token, sep="")
+                return(urll)
         }
-        else{
+        else {
                 return(NULL)
         }
 }
 
+## Function that performs the query and resolves the pagination tokens
+# url_query: url with the parameters to search
+places_query<-function(url_query){
+        iter<-0
+        term_cond<-""
+        sdoc<-list()
+        data<-list()
+        #x<-list()
+        url_query<-url_query
+        while(!is.null(term_cond)){
+                iter<-iter+1
+                #x[[iter]]<-url_query
+                sdoc[[iter]]<-getURL(url_query) # EL PROBLEMA ESTA EN QUE NO ACTUALIZA ESTA URL
+                data[[iter]]<-fromJSON(sdoc[[iter]])
+                term_cond<-data[[iter]]$next_page_token
+                url_query<-next_page(data[[iter]])
+                #print(url_query)
+                #if(iter==11){
+                #        break
+                #}
+        }
+        return(x)
+}
 
 key<-"AIzaSyCIsAa6qFbyca8ntlIjZTPedtGGAos-R8s"
-i<-54
+i<-65
 lat<-points_coords[i,2]
 long<-points_coords[i,1]
-url<-format_url(lat,long,radius=500,type="bank",key)
-sdoc<-list()
-data<-list()
-sdoc[[1]]<-getURL(url)
-data[[1]]<-fromJSON(sdoc[[1]])
+urll<-format_url(lat,long,radius=5000,type="bank",key)
+data<-places_query(urll)
 
+
+
+
+data[3]
+
+
+data[[4]]$next_page_token
+
+term_cond<-data[[1]]$next_page_token
 
 next_page(data[[1]])
 
+xxx<-lapply(data,function(x){x$next_page_token})
+duplicated(xxx)
+data[[1]]$results$name
+
+data[[1]]$results
 
 
 
+
+sdoc[[1]]<-getURL(url)
+data[[1]]<-fromJSON(sdoc[[1]])
+
+sdoc[[2]]<-getURL(next_page(data[[1]]))
+data[[2]]<-fromJSON(sdoc[[2]])
+
+sdoc[[3]]<-getURL(next_page(data[[2]]))
+data[[3]]<-fromJSON(sdoc[[3]])
+
+
+next_page(data[[2]])
 
 for i in nrow(points){
         lat<-points_coords[i,2]
